df4157558b9bb9bb10881d2761264245f174d646
wesleytruong <wesleytruong@ucsb.edu>
2024-02-19 03:00:36 -0800

    removed extra import

--- frontend/src/tests/pages/MenuItemReview/MenuItemReviewCreatePage.test.js
+++ frontend/src/tests/pages/MenuItemReview/MenuItemReviewCreatePage.test.js
@@ -7,7 +7,6 @@ import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 import axios from "axios";
 import AxiosMockAdapter from "axios-mock-adapter";
-import { colors } from "@storybook/node-logger";
 
 const mockToast = jest.fn();
 jest.mock('react-toastify', () => {
